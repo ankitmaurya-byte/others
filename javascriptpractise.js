1d8a727810cd6263fb252511a01150ae864964f8
ankit <ankitmaurya2989@gmail.com>
2022-11-10 22:55:15 +0530

    complete

--- javascriptpractise.js
+++ javascriptpractise.js
@@ -44,12 +44,16 @@ canvas.height=window.innerHeight;
         }
     })
        
-    colorarr=[
-        "blue",
-        "yellow",
-        "red",
-        "green"
-    ]
+    colorarr=['#FF6633', '#FFB399', '#FF33FF', '#FFFF99', '#00B3E6', 
+    '#E6B333', '#3366E6', '#999966', '#99FF99', '#B34D4D',
+    '#80B300', '#809900', '#E6B3B3', '#6680B3', '#66991A', 
+    '#FF99E6', '#CCFF1A', '#FF1A66', '#E6331A', '#33FFCC',
+    '#66994D', '#B366CC', '#4D8000', '#B33300', '#CC80CC', 
+    '#66664D', '#991AFF', '#E666FF', '#4DB3FF', '#1AB399',
+    '#E666B3', '#33991A', '#CC9999', '#B3B31A', '#00E680', 
+    '#4D8066', '#809980', '#E6FF80', '#1AFF33', '#999933',
+    '#FF3380', '#CCCC00', '#66E64D', '#4D80CC', '#9900B3', 
+    '#E64D66', '#4DB380', '#FF4D4D', '#99E6E6', '#6666FF'];
     function circle(x,y,dx,dy,radius){
         this.x=x;
         this.y=y;
@@ -76,7 +80,7 @@ canvas.height=window.innerHeight;
 
             if(mouse.x-this.x<60 && this.x-mouse.x<60 && mouse.y-this.y<60 && this.y-mouse.y<60){
                 if(this.radius<50){
-                    this.radius+=5;
+                    this.radius+=3;
                 }
             }else if (this.radius>this.originalradius){
                 this.radius-=0.4;
